0efbec609634f8eb6b140e4ca71cfa92220d26b9
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-02-13 13:11:34 +0100

    nieuwe update

--- index.js
+++ index.js
@@ -42,3 +42,20 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+// EIGEN
+
+// const card = document.querySelector(".card > section")
+
+// const root = document.documentElement;
+
+// document.addEventListener("mousemove", (e) => {
+//     const x = e.clientX - window.innerWidth / 2;
+//     const y = e.clientY - window.innerHeight / 2;
+
+
+//     root.style.setProperty('--rotateX', `${y * 0.02}deg`);
+//     root.style.setProperty('--rotateY', `${x * 0.02}deg`);
+// });
+
+
